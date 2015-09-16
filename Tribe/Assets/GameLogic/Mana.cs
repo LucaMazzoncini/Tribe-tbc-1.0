6f9d0f4bb719520c26e55b66b3ac537702da9f09
LucaMazzoncini <mazzonciniluca@gmail.com>
2015-09-17 00:02:31 +0200

    .

---
+++ Tribe/Assets/GameLogic/Mana.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace GameLogic
+{
+    
+    class Mana
+    {
+        public const int initMana = 10;
+        Dictionary<Enums.Mana, int> valueList;
+        Dictionary<Enums.Mana, int> poolList;
+        public Dictionary<Enums.Mana, int> Init()
+        {
+
+            Dictionary<Enums.Mana, int> param = new Dictionary<Enums.Mana, int>();
+            param.Add(Enums.Mana.Earth, 0);
+            param.Add(Enums.Mana.Fire, 0);
+            param.Add(Enums.Mana.Water, 0);
+            param.Add(Enums.Mana.Life, 0);
+            param.Add(Enums.Mana.Death, 0);
+            return param;
+        }
+        public Mana()
+        {
+            valueList = Init();
+            poolList = Init();
+            Random random = new Random();
+            for (int i = 0; i < initMana; i++)
+            {
+                int randomNumber = random.Next(1, 6);
+                switch (randomNumber)
+                {
+                    case 1:
+                        if (valueList[Enums.Mana.Earth] > 2)
+                            i--;
+                        else
+                            valueList[Enums.Mana.Earth] += 1;
+                        break;
+                    case 2:
+                        if (valueList[Enums.Mana.Fire] > 2)
+                            i--;
+                        else
+                            valueList[Enums.Mana.Fire] += 1;
+                        break;
+                    case 3:
+                        if (valueList[Enums.Mana.Water] > 2)
+                            i--;
+                        else
+                            valueList[Enums.Mana.Water] += 1;
+                        break;
+                    case 4:
+                        if (valueList[Enums.Mana.Life] > 2)
+                            i--;
+                        else
+                            valueList[Enums.Mana.Life] += 1;
+                        break;
+                    case 5:
+                        if (valueList[Enums.Mana.Death] > 2)
+                            i--;
+                        else
+                            valueList[Enums.Mana.Death] += 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+}
