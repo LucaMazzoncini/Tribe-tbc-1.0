6ba1b32c0ba0064fc1ac206e0994b014c7cae014
LucaMazzoncini <mazzonciniluca@gmail.com>
2015-09-26 06:19:08 +0200

    Con il tasto a modo

---
+++ Tribe/Assets/P2Script.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P2Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnMouseDown()
+    {
+        this.GetComponent<Animator>().Play("gigi");
+    }
+}
