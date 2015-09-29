7476738db1a30367b5fba89b90d4b33420d432c7
LucaMazzoncini <mazzonciniluca@gmail.com>
2015-09-29 21:16:49 +0200

    Mana Animation

--- Tribe/Assets/Editor/FBXScaleFix.cs
+++ Tribe/Assets/Editor/FBXScaleFix.cs
@@ -7,7 +7,7 @@ public class FBXScaleFix : AssetPostprocessor
 {
     public void OnPreprocessModel()
     {
-        ModelImporter modelImporter = (ModelImporter)assetImporter;
-        modelImporter.globalScale = 100;
+        //ModelImporter modelImporter = (ModelImporter)assetImporter;
+        //modelImporter.globalScale = 100;
     }
 }
