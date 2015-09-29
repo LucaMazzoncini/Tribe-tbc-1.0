b2a78234eca96836c752b289cf2fe03c3aa231d7
LucaMazzoncini <mazzonciniluca@gmail.com>
2015-09-29 21:18:35 +0200

    Mana Animation

---
+++ Tribe/Assets/UnitySceneAndScript/Board/Player/UpdateManaScriptP3.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateManaScriptP3 : MonoBehaviour {
+
+    public static bool update = false;
+    private static string value = "";
+    public void Update()
+    {
+        if (update)
+        {
+            if (transform.FindChild("ManaText").GetComponent<TextMesh>().text != value)
+            {
+                this.GetComponent<Animator>().Play("on");
+                transform.FindChild("ManaText").GetComponent<TextMesh>().text = value;
+            }
+            update = false;
+        }
+    }
+
+    public static void UpdateStatus(string mana)
+    {
+        value = mana;
+        update = true;
+    }
+}
