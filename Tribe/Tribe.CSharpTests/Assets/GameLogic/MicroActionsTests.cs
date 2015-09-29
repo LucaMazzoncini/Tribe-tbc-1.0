fb1a07555d29e32cb8112ba8aa4bf72482ad1f1d
LucaMazzoncini <mazzonciniluca@gmail.com>
2015-09-29 11:23:02 +0200

    Test di getTargets

---
+++ Tribe/Tribe.CSharpTests/Assets/GameLogic/MicroActionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic.Tests
+{
+    [TestClass()]
+    public class MicroActionsTests
+    {
+        [TestMethod()]
+        public void getTargetsTest()
+        {
+            string action = "Armor.2 Heal.2 Damage.1 AddMana Asleep";
+            List<Enums.Target> targets = MicroActions.getTargets(action);
+
+            Assert.IsTrue(targets.Count > 0);
+        }
+    }
+}
