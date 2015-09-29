fbb3b5a1b9ff987407c34f83294fc433d1729a17
Avurzo <sono.kindergarten@gmail.com>
2015-09-30 00:20:34 +0200

    creata classe "Spirit" (proprietà)

---
+++ Tribe/Assets/GameLogic/Spirit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    class Spirit : Card
+    {
+        public Spirit(string name) : base(string name) { }
+        public int essence;
+        public List<string> onAppear;
+        public List<string> onDeath;
+    }
+}
