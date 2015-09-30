0eb172a47db8f5258ca580967e305dc2ff030e83
Avurzo <sono.kindergarten@gmail.com>
2015-09-30 02:26:22 +0200

    modifiche varie. implementata funzione "getCardByName(string name)"

--- Tribe/Assets/GameLogic/Card.cs
+++ Tribe/Assets/GameLogic/Card.cs
@@ -11,7 +11,7 @@ namespace GameLogic
         public string name;
         public int id; //identificatore univoco che viene valorizzato quando entra in gioco
         public List<Power> powers;
-        public List<Enums.Mana> cost;
+        public Dictionary<Enums.Mana, int> manaCost;
         public Enums.Type type;
         public Enums.SubType subtype;
         public Enums.Target target; //se non puo' essere targettata va' messo a null
@@ -29,6 +29,42 @@ namespace GameLogic
         public void play() 
         {
             //paga il costo della carta
+        }
+        public Card InitFromInvocation(Invocation InvTemp)
+        {
+            this.name = InvTemp.name;
+            this.manaCost = InvTemp.manaCost;
+            this.type = InvTemp.type[0];
+            this.subtype = InvTemp.subType[0];
+            this.powers = InvTemp.powers;
+            if (this.type == Enums.Type.Elemental)
+            {
+                Elemental ElemTemp = new Elemental(this.name);
+                ElemTemp = (Elemental)this;
+                ElemTemp.strength = InvTemp.strength;
+                ElemTemp.constitution = InvTemp.constitution;
+                ElemTemp.hp = ElemTemp.constitution;
+                ElemTemp.rank = InvTemp.rank;
+                if (ElemTemp.rank > 0)
+                    ElemTemp.from = InvTemp.from;
+                ElemTemp.role = InvTemp.role;
+                ElemTemp.properties = InvTemp.properties;
+                ElemTemp.onAppear = InvTemp.onAppear;
+                ElemTemp.onDeath = InvTemp.onDeath;
+                return ElemTemp;
+            }
+            if (this.type == Enums.Type.Spirit)
+            {
+                Spirit SpiritTemp = new Spirit(this.name);
+                SpiritTemp = (Spirit)this;
+                SpiritTemp.essence = InvTemp.essence;
+                SpiritTemp.onAppear = InvTemp.onAppear;
+                SpiritTemp.onDeath = InvTemp.onDeath;
+                return SpiritTemp;
+            }
+            return this;
+        }
+
         }
 
         public void processMicroaction(List<string> paramList) //questa funzione processa e prepara le microazioni
