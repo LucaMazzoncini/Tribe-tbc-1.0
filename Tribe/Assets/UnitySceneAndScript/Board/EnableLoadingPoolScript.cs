91a4c05cd7c290b7583c61bb23966bdb39fd32bb
LucaMazzoncini <mazzonciniluca@gmail.com>
2015-10-02 11:00:29 +0200

    .

---
+++ Tribe/Assets/UnitySceneAndScript/Board/EnableLoadingPoolScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameEventManagement;
+
+public class EnableLoadingPoolScript : MonoBehaviour {
+
+    public string manaParam = "";
+    public void SetLoadingPool(bool param,string mana)
+    {
+        manaParam = mana;
+        transform.FindChild("LoadingPool").gameObject.SetActive(param);
+
+        if (transform.FindChild("LoadingPool").gameObject.activeSelf)
+        if (transform.FindChild("LoadingPool").GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Base.Particelle_loading"))
+        {
+            //transform.FindChild("LoadingPool").gameObject.SetActive(false);
+            Debug.Log("E' in Particelle_loading");
+        }
+
+
+
+    }
+
+    public void Update()
+    {
+        if(transform.FindChild("LoadingPool").gameObject.activeSelf)
+        if (transform.FindChild("LoadingPool").GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Base.Idle"))
+        {
+            GameEventManager.CreatePool(manaParam);
+            transform.FindChild("LoadingPool").gameObject.SetActive(false);
+            
+        }
+    }
+
+}
---
+++ Tribe/Assets/UnitySceneAndScript/Board/LoadedScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameEventManagement;
+
+public class LoadedScript : MonoBehaviour {
+
+    public GameObject selfObj;
+    private static bool destroy = false;
+	// Use this for initialization
+	void Start () {
+        GameEventManager.loaded += GameEventManager_loaded;
+        selfObj.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(destroy)
+            selfObj.SetActive(false);
+    }
+
+    public static void GameEventManager_loaded()
+    {
+        destroy = true;
+    }
+}
