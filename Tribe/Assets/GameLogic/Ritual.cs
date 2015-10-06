ebf1b7e64b0ae1277ffbd3aa683d7aa61f65f76d
Avurzo <sono.kindergarten@gmail.com>
2015-10-06 17:17:10 +0200

    creata classe ritual.

---
+++ Tribe/Assets/GameLogic/Ritual.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    public class Ritual : Card
+    {
+        public Ritual(string param) : base(param) { }
+    }
+}
