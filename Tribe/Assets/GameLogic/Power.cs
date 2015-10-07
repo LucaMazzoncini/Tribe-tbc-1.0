4a55762db4ecd8fd4ae984f2938873a0889087d8
Avurzo <sono.kindergarten@gmail.com>
2015-10-07 06:14:25 +0200

    classe Power separata dal resto.

---
+++ Tribe/Assets/GameLogic/Power.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    public class Power
+        {
+            public int cooldown;
+            public List<string> microActions;
+            public Power()
+            {
+                cooldown = 0;
+                microActions = new List<string>();
+            }
+        }
+}
