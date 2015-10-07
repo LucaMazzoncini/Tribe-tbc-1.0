492a790ce6922101d9f2bb1dc1ba2287e445c1b1
Avurzo <sono.kindergarten@gmail.com>
2015-10-07 15:29:43 +0200

    la target segata in precedenza

---
+++ Tribe/Assets/GameLogic/Target.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    public class Target
+    {
+        public Enums.Target target;
+        public int id = 0;
+        public string name = "";
+    }
+}
